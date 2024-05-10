bee9ec73df7c49251b8353a798b8305fad198eb1
Samanta <samantamendez96@gmail.com>
2024-05-10 19:50:41 +0200

    commit my final version of exam

--- src/app.test.js
+++ src/app.test.js
@@ -1,10 +1,92 @@
 import {jest, test, expect, describe} from "@jest/globals"; // this is optional, all three are global variables im runner scope
+import app from './app.js';
+import request from 'supertest';
+import {x} from "joi";
 
-describe('api test for the quiz', () => {
+let token;
 
-    test('placeholder test, please remove ...', () => {
-        expect(true).toBeTruthy();
-    })
+beforeAll(async () => {
+    const authResponse = await request(app)
+        .post('/authenticate')
+        .auth('Max', '123')
+        .expect(200);
+
+    token = authResponse.body.token;
+});
+describe('api test', () => {
+
+    test('POST /authenticate', async () => {
+        const response = await request(app)
+            .post('/authenticate')
+            .auth('Max', '123')
+            .expect(200);
+
+        expect(response.body).toHaveProperty('token');
+    });
+
+    test('GET /questions', async () => {
+        const response = await request(app)
+            .get('/questions')
+            .expect(200);
+
+        expect(response.body).toBeInstanceOf(Array);
+    });
+
+    test('GET /questions/{questionId}', async () => {
+        const response = await request(app)
+            .get('/questions/544db309-40cf-4dd8-8662-c10ed3502a5d')
+            .expect(200);
+
+        expect(response.body).toHaveProperty('id', '544db309-40cf-4dd8-8662-c10ed3502a5d');
+    });
+
+    test('POST /game-runs', async () => {
+        const response = await request(app)
+            .post('/game-runs')
+            .set('Authorization', `Bearer ${token}`)
+            .expect(201);
+
+        expect(response.body).toHaveProperty('runId');
+        expect(response.body).toHaveProperty('userName');
+    });
+
+    test('PUT /game-runs/{runId}/responses', async () => {
+
+        const gameRunResponse = await request(app)
+            .post('/game-runs')
+            .set('Authorization', `Bearer ${token}`)
+            .expect(201);
+
+        const { runId } = gameRunResponse.body;
+
+
+        const response = await request(app)
+            .put(`/game-runs/${runId}/responses`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({ questionId: '544db309-40cf-4dd8-8662-c10ed3502a5d', answerIndex: '0' })
+            .expect(200);
+
+        expect(response.body.responses).toHaveProperty('544db309-40cf-4dd8-8662-c10ed3502a5d', '0');
+    });
+
+    test('GET /game-runs/{runId}/results', async () => {
+
+        const gameRunResponse = await request(app)
+            .post('/game-runs')
+            .set('Authorization', `Bearer ${token}`)
+            .expect(201);
+
+        const { runId } = gameRunResponse.body;
+
+        const response = await request(app)
+            .get(`/game-runs/${runId}/results`)
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200);
+
+        expect(response.body).toHaveProperty('id', runId);
+        expect(response.body).toHaveProperty('responses');
+    });
+
 
 });
 
